417f45b8ac22dfaf6f0296c986b5f5f98ac03d9c
gideonwolfe <wolfeg2@wwu.edu>
2021-11-07 00:36:13 -0700

    update script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,10 @@ set -e
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/GideonWolfe/arch-bootstrap/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="paru"
 [ -z "$repobranch" ] && repobranch="master"
+[ -z "$wallpaperrepo" ] && wallpaperrepo="https://github.com/GideonWolfe/wallpapers.git"
 [ -z "$name" ] && name="gideon"
 
+export repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
 
 ### FUNCTIONS ###
 
@@ -14,10 +16,30 @@ set -e
 installpkgsilent(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 # I like to see some output
 installpkg(){ pacman --noconfirm --needed -S "$1" 2>&1 ;}
+# Install aur package
+aurinstall() { \
+	echo "$aurinstalled" | grep -q "^$1$" && return 1
+	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
+}
+# Install python package
+pipinstall() { \
+	[ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
+	yes | pip install "$1"
+}
+# Install node package
+npminstall() { \
+	[ -x "$(command -v "npm")" ] || installpkg npm >/dev/null 2>&1
+	#npm install "$1"
+	sudo -u "$name" npm install -g "$1"
+}
 
+
+
+# Generic error function
 error() { printf "%s\n" "$1" >&2; exit 1; }
 
-manualinstall() { # Installs $1 manually. Used only for AUR helper here.
+# Installs $1 manually. Used only for AUR helper here.
+manualinstall() {
 	# Should be run after repodir is created and var is set.
 	sudo -u "$name" mkdir -p "$repodir/$1"
 	#sudo -u "$name" git clone --depth 1 "https://aur.archlinux.org/$1.git" "$repodir/$1" >/dev/null 2>&1 ||
@@ -28,15 +50,29 @@ manualinstall() { # Installs $1 manually. Used only for AUR helper here.
   sudo -u "$name" -D "$repodir/$1" makepkg --noconfirm -si 2>&1 || return 1
 }
 
+
+installationloop() { \
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
+	total=$(wc -l < /tmp/progs.csv)
+	aurinstalled=$(pacman -Qqm)
+	while IFS=, read -r tag program comment; do
+		n=$((n+1))
+		echo "$comment" | grep -q "^\".*\"$" && comment="$(echo "$comment" | sed "s/\(^\"\|\"$\)//g")"
+		case "$tag" in
+			"A") aurinstall "$program" "$comment" ;;
+			"P") pipinstall "$program" "$comment" ;;
+			"N") npminstall "$program" "$comment" ;;
+			*) installpkg "$program" "$comment" ;;
+		esac
+	done < /tmp/progs.csv ;
+}
+
 drawTitle() {
   figlet $1
 }
 
 
 
-
-
-export repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
 
 
 ## Tasks
@@ -46,8 +82,12 @@ drawTitle "System Tweaks"
 # Use all cores for compilation.
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 # Pacman config
+# enable ilovecandy
 grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf || error "Failed to enable ILoveCandy"
-sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/" /etc/pacman.conf || error "Failed to enable Parallel Downloads and color"
+# Enable parallel downloads and color support
+sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 8/;s/^#Color$/Color/" /etc/pacman.conf || error "Failed to enable Parallel Downloads and color"
+# Enable verbose package lists
+sed -i "s/^#VerbosePkgLists/VerbosePkgLists/" /etc/makepkg.conf
 
 # make sure we have the bare minimum
 for x in curl ca-certificates base-devel git ntp fish figlet; do
@@ -65,21 +105,50 @@ drawTitle "Install ${aurhelper}"
 
 # Install packages
 
-# Install nvim setup
-
 # Install dotfiles
 drawTitle "Cloning Dotfiles"
-git clone $dotfilesrepo /home/$name/dotfiles
-
-# Create ~/.local/bin/* dirs
+# git clone $dotfilesrepo /home/$name/dotfiles
 
 # Setup docker
 drawTitle "Docker Setup"
 usermod -aG docker $name
-systemctl enable docker.service
-systemctl enable containerd.service
 
 # Setup permissions
+
+
+# Create desired folders
+drawTitle "Creating Directories"
+sudo -u "$name" mkdir -p "/home/$name/photos/screenshots"
+sudo -u "$name" mkdir -p "/home/$name/programs/"
+sudo -u "$name" mkdir -p "/home/$name/projects/"
+sudo -u "$name" mkdir -p "/home/$name/documents/"
+sudo -u "$name" mkdir -p "/home/$name/downloads/"
+sudo -u "$name" mkdir -p "/home/$name/.local/bin/"
+
+# Install nvim setup
+# sudo -u "$name" git clone https://github.com/GideonWolfe/nvim-lua/ "/home/$name/programs/nvim-lua"
+
+# Download wallpapers
+drawTitle "Downloading Wallpapers"
+# sudo -u "$name" git clone $wallpaperrepo "/home/$name/photos/wallpapers"
+
+
+# Enable services
+drawTitle "Enabling Services"
+# systemctl enable lightdm
+# systemctl enable NetworkManager
+# systemctl enable bluetooth
+# systemctl enable docker
+# systemctl enable containerd
+# systemctl enable sshd
+# systemctl enable ckb-next-daemon
+# systemctl enable cups
+
+# Sync time
+timedatectl set-ntp true
+
+installationloop
+
 
 # Non packaged programs:
 # zathura pywal
