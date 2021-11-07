15b76e54dbbffb5cbf12a38dcea4e0d50063c7b8
gideonwolfe <wolfeg2@wwu.edu>
2021-11-07 12:45:52 -0800

    progress

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,11 +15,15 @@ export repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":w
 # Silent pacman install
 installpkgsilent(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 # I like to see some output
+# --noconfirm seemed to not work when confirming rustup dependency replacement?
 installpkg(){ pacman --noconfirm --needed -S "$1" 2>&1 ;}
 # Install aur package
 aurinstall() { \
-	echo "$aurinstalled" | grep -q "^$1$" && return 1
-	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
+	# echo "$aurinstalled" | grep -q "^$1$" && return 1
+  echo "[AUR] Installing " "$1"
+  echo $1
+	# sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
+	sudo -u "$name" $aurhelper -S --noconfirm "$1"
 }
 # Install python package
 pipinstall() { \
@@ -30,6 +34,7 @@ pipinstall() { \
 npminstall() { \
 	[ -x "$(command -v "npm")" ] || installpkg npm >/dev/null 2>&1
 	#npm install "$1"
+  echo INSTALLING NPM PACKAGE
 	sudo -u "$name" npm install -g "$1"
 }
 
@@ -62,7 +67,24 @@ installationloop() { \
 			"A") aurinstall "$program" "$comment" ;;
 			"P") pipinstall "$program" "$comment" ;;
 			"N") npminstall "$program" "$comment" ;;
-			*) installpkg "$program" "$comment" ;;
+			*) installpkgsilent "$program" "$comment" ;;
+		esac
+	done < /tmp/progs.csv ;
+}
+
+
+installationloop2() { \
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
+	total=$(wc -l < /tmp/progs.csv)
+	aurinstalled=$(pacman -Qqm)
+	while IFS=, read -r tag program comment; do
+		n=$((n+1))
+		echo "$comment" | grep -q "^\".*\"$" && comment="$(echo "$comment" | sed "s/\(^\"\|\"$\)//g")"
+		case "$tag" in
+			"A") echo AUR "$program" "$comment" ;;
+			"P") echo PIP "$program" "$comment" ;;
+			"N") echo NODE "$program" "$comment" ;;
+			*) echo "$program" "$comment" ;;
 		esac
 	done < /tmp/progs.csv ;
 }
@@ -88,36 +110,41 @@ grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy"
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 8/;s/^#Color$/Color/" /etc/pacman.conf || error "Failed to enable Parallel Downloads and color"
 # Enable verbose package lists
 sed -i "s/^#VerbosePkgLists/VerbosePkgLists/" /etc/makepkg.conf
+# Enable multilib
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 # make sure we have the bare minimum
-for x in curl ca-certificates base-devel git ntp fish figlet; do
+for x in curl ca-certificates base-devel git ntp fish figlet python-pip npm; do
   # dialog --title "Auto Installation" --infobox "Installing \`$x\` which is required to install and configure other programs." 5 70
-  installpkg "$x"
+  # installpkg "$x"
+  installpkgsilent "$x"
 done
 
+installationloop
+
 # Set default shell
-drawTitle "Change Shell"
-chsh -s /usr/bin/fish "$name" 2>&1
+# drawTitle "Change Shell"
+# chsh -s /usr/bin/fish "$name" 2>&1
 
 # Install yay/paru
-drawTitle "Install ${aurhelper}"
+# drawTitle "Install ${aurhelper}"
 #manualinstall paru || error "Failed to installAUR Helper"
 
 # Install packages
 
 # Install dotfiles
-drawTitle "Cloning Dotfiles"
+# drawTitle "Cloning Dotfiles"
 # git clone $dotfilesrepo /home/$name/dotfiles
 
 # Setup docker
-drawTitle "Docker Setup"
+# drawTitle "Docker Setup"
 usermod -aG docker $name
 
 # Setup permissions
 
 
 # Create desired folders
-drawTitle "Creating Directories"
+# drawTitle "Creating Directories"
 sudo -u "$name" mkdir -p "/home/$name/photos/screenshots"
 sudo -u "$name" mkdir -p "/home/$name/programs/"
 sudo -u "$name" mkdir -p "/home/$name/projects/"
@@ -129,12 +156,12 @@ sudo -u "$name" mkdir -p "/home/$name/.local/bin/"
 # sudo -u "$name" git clone https://github.com/GideonWolfe/nvim-lua/ "/home/$name/programs/nvim-lua"
 
 # Download wallpapers
-drawTitle "Downloading Wallpapers"
+# drawTitle "Downloading Wallpapers"
 # sudo -u "$name" git clone $wallpaperrepo "/home/$name/photos/wallpapers"
 
 
 # Enable services
-drawTitle "Enabling Services"
+# drawTitle "Enabling Services"
 # systemctl enable lightdm
 # systemctl enable NetworkManager
 # systemctl enable bluetooth
@@ -147,7 +174,6 @@ drawTitle "Enabling Services"
 # Sync time
 timedatectl set-ntp true
 
-installationloop
 
 
 # Non packaged programs:
